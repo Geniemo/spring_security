201deb478c875618cda42c163362deb42f15c218
geniemo <kor98won@gmail.com>
2022-08-16 00:06:15 +0900

    스프링 시큐리티 OAuth 2.0 - 네이버 로그인 완료

--- src/main/java/com/cos/security1/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/oauth/PrincipalOauth2UserService.java
@@ -4,6 +4,7 @@ import com.cos.security1.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.oauth.provider.FacebookUserInfo;
 import com.cos.security1.oauth.provider.GoogleUserInfo;
+import com.cos.security1.oauth.provider.NaverUserInfo;
 import com.cos.security1.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -60,8 +63,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
         } else {
-            System.out.println("구글과 페이스북만 지원");
+            System.out.println("구글과 페이스북과 네이버만 지원");
         }
         String provider = oAuth2UserInfo.getProvider();
         String providerId = oAuth2UserInfo.getProviderId(); // google 기준으로 적은 것이기 때문에 null 로 나온다. 페이스북은 id 로 온다.
